8e3e0330ac41d63da099f56bdafbf58767e772ca
joaovitorldm <joaovitorlemosdemiranda@gmail.com>
2024-10-21 18:39:16 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var nome = prompt ('Qual o seu nome?')
+alert ('Obrigado por acessar o meu site ' + nome + '!')
+var n1 = Number (prompt ('Escolha um número'))
+var n2 = Number (prompt ('Escolha outro número'))
+var n3 = n1 + n2
+alert (`A soma entre os números ${n1} e ${n2} é igual a ${n3}`)
